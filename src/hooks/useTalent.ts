5161a80b84d3c7777cec0f0a230f78cc178ccd3e
Ash Goren <ash@mortalwombat.net>
2025-07-30 00:25:21 -0700

    refactor useTalent hook

--- src/hooks/useTalent.ts
+++ src/hooks/useTalent.ts
@@ -6,91 +6,99 @@ import { SOUND_GUARANTEE, GEAR_RENTAL, MAX_SHARES_PER_ROLE } from 'src/config';
 import type { WorksheetFormData } from 'types/worksheet';
 
 export const useTalent = () => {
+  // 1. Parse Raw Values
   const { control } = useFormContext<WorksheetFormData>();
   const { admissions, miscExpenses } = useFinancials();
-
   const [rawTalent, rawGuarantee, rawRent, gearRental] = useWatch({
     name: ['talent', 'guarantee', 'rent', 'gearRental'],
     control
   });
-
   const rent = useMemo(() => Number(rawRent) || 0, [rawRent]);
-  const guarantee = useMemo(() => Number(rawGuarantee) || 0, [rawGuarantee]);
-
-  const baseSoundGuarantee = useMemo(() => gearRental ? SOUND_GUARANTEE - GEAR_RENTAL : SOUND_GUARANTEE, [gearRental]);
-
-  const guarantees: Record<string, number> = useMemo(() => ({
-    ...calculatePortions(guarantee, rawTalent),
-    sound: calculatePortions(baseSoundGuarantee, rawTalent).sound
-  }), [guarantee, rawTalent, baseSoundGuarantee]);
-
+  const pcdcGuarantee = useMemo(() => Number(rawGuarantee) || 0, [rawGuarantee]);
   const talent = useMemo(() => rawTalent.map((t) => ({
-    name: t.name,
-    role: t.role,
-    travel: Number(t.travel) || 0,
+    ...t,
+    travel: Number(t.travel) || 0
+  })), [rawTalent]);
+
+  // 2. Calculate Guarantees
+  const guarantees = useTalentGuarantees(talent, pcdcGuarantee, gearRental);
+  const talentWithGuarantees = useMemo(() => talent.map((t) => ({
+    ...t,
     guarantee: guarantees[t.role],
-    share: null, // will be calculated later
-    totalPay: null // will be calculated later
-  })), [rawTalent, guarantees]);
+  })), [talent, guarantees]);
 
-  const result = useMemo(() => {
+  // 3. Calculate Pay Basis
+  const payBasis = usePayBasis(talentWithGuarantees, admissions, miscExpenses, rent, pcdcGuarantee);
+
+  // 4. Calculate Shares
+  const { pcdcShare, shares } = useTalentShares(talentWithGuarantees, payBasis);
+  const talentWithShares = useMemo(() => talentWithGuarantees.map((t) => ({
+    ...t,
+    share: t.role === 'sound' ? 0 : shares[t.role]
+  })), [talentWithGuarantees, shares]);
+
+  // 5. Calculate Total Pay
+  const finalTalent = useMemo(() => talentWithShares.map((t) => ({
+    ...t,
+    totalPay: t.travel + t.guarantee + t.share
+  })), [talentWithShares]);
+
+  return {
+    talent: finalTalent,
+    payBasis,
+    pcdcShare
+  };
+};
+
+const useTalentGuarantees = (
+  talent: { role: string }[],
+  guarantee: number,
+  gearRental: boolean
+): Record<string, number> => {
+  const baseSoundGuarantee = useMemo(() => gearRental ? SOUND_GUARANTEE - GEAR_RENTAL : SOUND_GUARANTEE, [gearRental]);
+  const guarantees: Record<string, number> = useMemo(() => ({
+    ...calculatePortions(guarantee, talent),
+    sound: calculatePortions(baseSoundGuarantee, talent).sound
+  }), [talent, guarantee, baseSoundGuarantee]);
+  return guarantees;
+};
+
+const usePayBasis = (
+  talent: { travel: number, guarantee: number }[],
+  admissions: number | null,
+  miscExpenses: number,
+  rent: number,
+  pcdcGuarantee: number
+): number | null => {
+  return useMemo(() => {
+    if (!isNum(admissions)) return null;
+
     const totalTravel = talent.reduce((sum, t) => sum + t.travel, 0);
-    const totalGuarantee = talent.reduce((sum, t) => sum + t.guarantee, 0) + guarantee; // include PCDC's 90
+    const totalGuarantee = talent.reduce((sum, t) => sum + t.guarantee, 0) + pcdcGuarantee; // PCDC also gets a 90 guarantee
 
-    const payBasis =
-      isNum(admissions) && isNum(miscExpenses) && isNum(rent) && isNum(totalGuarantee) && isNum(totalTravel)
-        ? admissions - rent - miscExpenses - totalGuarantee - totalTravel
-        : null;
+    return admissions - rent - miscExpenses - totalGuarantee - totalTravel;
+  }, [talent, admissions, miscExpenses, rent, pcdcGuarantee]);
+};
 
-    if (!isNum(payBasis)) { // not yet ready to calculate things
-      console.log('useTalent: payBasis is not a number');
-      return {
-        talent,
-        payBasis: null,
-        talentShare: null
-      };
-    }
+const useTalentShares = (
+  talent: { role: string }[],
+  payBasis: number | null
+): { pcdcShare: number | null, shares: Record<string, number> } => {
+  return useMemo(() => {
+    if (payBasis === null) return { pcdcShare: null, shares: {} };
+    if (payBasis <= 0) { return { pcdcShare: 0, shares: { sound: 0, caller: 0, musician: 0 } }; }
 
-    if (payBasis <= 0) { // no shares
-      console.log('useTalent: payBasis is zero or negative');
-      return {
-        talent: talent.map(t => ({
-          ...t,
-          share: 0,
-          totalPay: t.travel + t.guarantee
-        })),
-        payBasis,
-        talentShare: 0
-      };
-    }
+    const numCallers = talent.filter(t => t.role === 'caller').length;
+    const numMusicians = talent.filter(t => t.role === 'musician').length;
+    const numCallerShares = Math.min(MAX_SHARES_PER_ROLE.caller, numCallers);
+    const numMusicianShares = Math.min(MAX_SHARES_PER_ROLE.musician, numMusicians);
+    const numShares = numCallerShares + numMusicianShares + 1; // PCDC also gets one share
 
-    // at this point we know payBasis is a positive number, which means share below is guaranteed to be a positive number too
-
-    const numCallerShares = Math.min(MAX_SHARES_PER_ROLE.caller, talent.filter(t => t.role === 'caller').length);
-    const numMusicianShares = Math.min(MAX_SHARES_PER_ROLE.musician, talent.filter(t => t.role === 'musician').length);
-    const numShares = numCallerShares + numMusicianShares + 1; // pcdc gets a share too
     const pcdcShare = payBasis / numShares;
-    console.log('pcdcShare', pcdcShare, 'numShares', numShares, 'payBasis', payBasis);
     const shares: Record<string, number> = calculatePortions(pcdcShare, talent);
 
-    const talentWithShares = talent.map((t) => {
-      const share = t.role === 'sound' ? 0 : shares[t.role];
-      const totalPay = t.travel + t.guarantee + share;
-      return {
-        ...t,
-        share,
-        totalPay
-      };
-    });
-
-    return {
-      talent: talentWithShares,
-      payBasis,
-      pcdcShare
-    };
-  }, [talent, admissions, miscExpenses, rent, guarantee]);
-
-  return result;
+    return { pcdcShare, shares };
+  }, [payBasis, talent]);
 };
 
 const calculatePortions = (amount: number, talent: { role: string }[]) => {
