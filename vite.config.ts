30f252c8256d28660cad578a08237d503ee61cd3
Ash Goren <ash@mortalwombat.net>
2025-10-17 01:21:59 -0700

    add PWA config for offline support

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import eslint from 'vite-plugin-eslint2'
+import { VitePWA } from 'vite-plugin-pwa'
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    // visualizer({ open: true }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // 5 MB
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'PCDC Contra Worksheet',
+        short_name: 'Worksheet',
+        description: 'PCDC Contra Worksheet',
+        theme_color: '#ffffff',
+        icons: [
+          {
+          src: '/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any' // android
+          }
+        ]
+      }
+    }),
     eslint({
       include: ['src/**/*.{js,jsx,ts,tsx}'],
       emitWarning: true,
