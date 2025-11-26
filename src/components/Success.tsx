3ce2bb89e1e97cfb29fa1a4abb31123f5ec3b648
Ash Goren <ash@mortalwombat.net>
2025-11-26 00:25:28 -0800

    add bookkeeper & membership contact info to success page

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -1,7 +1,30 @@
-import { Alert} from '@mui/material';
+import { Alert, Typography } from '@mui/material';
+const { VITE_BOOKKEEPER_CONTACT, VITE_MEMBERSHIP_CONTACT } = import.meta.env;
 
 export const Success = () => {
   return (
-    <Alert severity='success'>Worksheet submitted!</Alert>
+    <>
+      <Alert severity='success'>Worksheet submitted!</Alert>
+
+      <Typography sx={{ my: 3, ml: 2 }}>
+        <strong>Mail checks to:</strong><br />
+        {parseContact(VITE_BOOKKEEPER_CONTACT)}
+      </Typography>
+      <Typography sx={{ my: 3, ml: 2 }}>
+        <strong>Mail membership forms to:</strong><br />
+        {parseContact(VITE_MEMBERSHIP_CONTACT)}
+      </Typography>
+    </>
+  );
+};
+
+const parseContact = (contact: string) => {
+  const parts = contact.split(',');
+  return (
+    <>
+      {parts[0]}<br />
+      {parts.slice(1,-1).join(',')}<br />
+      {parts.slice(-1)}
+    </>
   );
 };
